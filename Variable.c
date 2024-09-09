a02c74a0f677c29ca3a1289dedb532e065dcf22d
LianggaRistiana <liangga4@gmail.com>
2024-09-09 18:56:43 +0800

    test

--- Variable.c
+++ Variable.c
@@ -3,6 +3,12 @@
 int main(int argc, char const *argv[])
 {
     char name[100];
+    char name2[100];
+    char name3[100];
+    char name4[100];
+    char name5[100];
+    char name6[100];
+    char name7[100];
 
     scanf("%s", name);
     printf("Hello, %s", name);
